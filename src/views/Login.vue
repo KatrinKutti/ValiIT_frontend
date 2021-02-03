f2d153113546561ec7bb45a8f93aff991ff6108c
Katrin Kutti <katrin.kutti@hotmail.com>
2021-02-03 15:17:49 +0200

    Vue algus

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    Username:<input v-model="userName" placeholder="Username"><br>
+    Password:<input v-model="pswrd" placeholder="Password"><br>
+    <button v-on:click="saveInHtml()">Submit</button>
+  </div>
+</template>
+
+<script>
+let saveInJs = function () {
+  alert('Login successful');
+}
+export default {
+  name: 'Login',
+  components: {},
+  data: function () {
+    return {
+      userName: '',
+      pswrd: '',
+    }
+  },
+  methods: {
+    saveInHtml: saveInJs
+  }
+}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    First name:<input v-model="customer.firstName" placeholder="First name"><br>
+    Last name:<input v-model="customer.lastName" placeholder="Last name"><br>
+    Address:<input v-model="customer.address" placeholder="Address"><br>
+    Username:<input v-model="customer.userName" placeholder="Username"><br>
+    Password:<input v-model="customer.pswrd" placeholder="Password"><br>
+    <button v-on:click="saveInHtml()">Submit</button>
+  </div>
+</template>
+
+<script>
+let saveInJs = function () {
+  this.$http.post('http://localhost:8080/Katrin/registerCustomer', this.customer);
+  alert('New customer registered!');
+}
+
+export default {
+  name: 'Register',
+  components: {},
+  data: function () {
+    return {
+     customer:{}
+    }
+  },
+  methods: {
+    saveInHtml: saveInJs
+  }
+}
+</script>
